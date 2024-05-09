c5d758e88e6ea375501322cb8f123d2f2f687339
michal-fityka <81447683+michal-fityka@users.noreply.github.com>
2024-05-09 20:43:42 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,2 +1,19 @@
 with open('file.md', 'w') as f:
-    f.write('To jest przykladowy tekst w pliku')
+    f.write("""# Scripts 
+## tenants
+To jest przykladowy tekst
+
+## backups
+To jest przykladowy tekst
+
+## github_cli
+To jest przykladowy tekst
+
+## aws
+To jest przykladowy tekst
+
+## azure
+To jest przykladowy tekst
+
+## gcp
+To jest przykladowy tekst""")
